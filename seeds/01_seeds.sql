e721fd85a3fa623a02ebaef15131db5741e1f8be
Sebastian Kann <sebastiankann@gmail.com>
2020-10-14 19:55:56 +0000

    data seeds written

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO users (name, email, password)
+VALUES ('Eva Stanley', 'sebastianguerra@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Louisa Meyer', 'jacksonrose@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Dominic Parks', 'victoriablackwell@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Sue Luna', 'jasonvincent@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Rosalie Garza', 'jacksondavid@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Etta West', 'chalielevy@yahoo.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Margaret Wong', 'makaylaweiss@iscloud.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Leroy Hart', 'jaycereynolds@inbox.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+
+
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)
+VALUES (1, 'Speed lamp', 'description', 'https://tse1.mm.bing.net/th?id=OIP.6F0Wjgo-M0n3RVjh_nNuZQHaFj&pid=Api', 'https://tse2.mm.bing.net/th?id=OIP.RjGQinvvru336AwLhnmGkgHaFj&pid=Api', 93061, 6, 4, 8, 'Canada', '546 Namsub Highway', 'Sotboske', 'Quebec', '28142'),
+(1, 'Blank corner', 'description', 'https://tse4.mm.bing.net/th?id=OIP._FEBEX3CkWs74ndv4T-_vAHaEK&pid=Api', 'https://tse3.mm.bing.net/th?id=OIP.I5moFLfFjExbzxAim-DLgQHaEo&pid=Api', 85324, 0, 5, 6, 'Canada', '1650 Hejto Central', 'Genzuj', 'Newfoundland and Labrador', '44583');
+
+
+
